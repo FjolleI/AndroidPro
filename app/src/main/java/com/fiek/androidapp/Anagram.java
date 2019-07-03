cfb07c018a04f2423f869bd1a496ab8b52744148
blendbunjaku <blendbunjaku8@gmail.com>
2019-07-03 03:01:19 +0200

    AnagramClass2

--- app/src/main/java/com/fiek/androidapp/Anagram.java
+++ app/src/main/java/com/fiek/androidapp/Anagram.java
@@ -7,6 +7,27 @@ public class Anagram {
     public static final String[] FJALET = {"ARRITJE", "AUDICION", "BARI", "DJE", "KAFSHE", "KIMIKE",
             "MESIM", "MOTI", "NDERMJET", "NESER", "NERVOZ", "PAGESE", "POLITIKE",
             "QEVERI", "SPITAL", "SOT", "SUBSTANCA",
-            "SHPEJTE", "TELEFON", "TOKA", "ZEZE",};
+            "SHPEJTE", "TELEFON", "TOKA", "ZEZE",  };
 
+
+    public static String randomFjala() {
+        return FJALET[RANDOM.nextInt(FJALET.length)];
+    }
+
+    public static String shuffleFjala(String fjala) {
+        if (fjala!=null && !"".equals(fjala)) {
+            char a[] = fjala.toCharArray();
+
+            for (int i=0; i<a.length ;i++) {
+                int j = RANDOM.nextInt(a.length);
+                char tmp = a[i];
+                a[i] = a[j];
+                a[j] = tmp;
+            }
+
+            return new String(a);
+        }
+
+        return fjala;
+    }
 }
