bc0e5d853e0bdc0dcae9a6def67bc69946028ebf
blendbunjaku <blendbunjaku8@gmail.com>
2019-07-05 16:41:56 +0200

    Merge branch 'master' of https://github.com/FjolleI/AndroidPro
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/vcs.xml
    #	app/src/main/java/com/fiek/androidapp/MainActivity.java

--- app/src/main/java/com/fiek/androidapp/MainActivity.java
+++ app/src/main/java/com/fiek/androidapp/MainActivity.java
@@ -1,7 +1,5 @@
 package com.fiek.androidapp;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
