a57f5640455e5d3ef4398fe35898181b20bcf6e5
Fjollei <fjollaislamaj@gmail.com>
2019-07-06 05:44:56 +0200

    Welcome

--- app/src/main/java/com/fiek/androidapp/Welcome.java
+++ app/src/main/java/com/fiek/androidapp/Welcome.java
@@ -1,6 +1,48 @@
 package com.fiek.androidapp;
 
-public class Welcome {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Welcome extends AppCompatActivity {
+
+    Button buttonStart;
+
+    private String data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_welcome);
 
 
+
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent s = new Intent(Welcome.this, MainActivity.class);
+                startActivity(s);
+            }
+        });
+
+
+        ImageButton search = (ImageButton) findViewById(R.id.imageButton);
+
+        search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent s = new Intent(Welcome.this, About.class);
+                startActivity(s);
+            }
+
+
+        });
+
+    }
 }
