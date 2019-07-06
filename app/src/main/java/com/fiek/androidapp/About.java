59eaf6538e9dee68b8d70b0d73cefaca61a53375
Fjollei <fjollaislamaj@gmail.com>
2019-07-06 05:59:31 +0200

    AboutClass3

--- app/src/main/java/com/fiek/androidapp/About.java
+++ app/src/main/java/com/fiek/androidapp/About.java
@@ -44,3 +44,28 @@ public class About extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(),"Feedback",Toast.LENGTH_LONG).show();
             }
         });
+
+
+        rlHelp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(About.this)
+                        .setTitle("HELP")
+                        .setMessage("Ju lutem prisni që të shfaqet një fjalë më shkronja të parenditura.\n" +
+                                "\n" +
+                                "Pasiqë të shfaqet anagrami ju duhet të shënoni fjalën e sakte që ju mendoni që e formojnë ato shkronja te parenditura.\n" +
+                                "\n" +
+                                "Pasiqë të shkruani fjalën që mendoni që është e saktë shtypni butonin \"VALIDO\"\n" +
+                                "Nese nuk është e saktë ajo fjalë që keni shkruar atëherë ju duhet te provoni përsëri, e në rast se e qëlloni fjalën e saktë atëherë ju ndërrohet fjala dhe ju numërohet si 1 pikë.\n" +
+                                "\n" +
+                                "Nëse assesi nuk mund të qëlloni fjalën e kërkuar që e keni të shfaqur atëherë ju keni mundësinë të rifilloni edhe njëhërë nga fillimi duke shtypur butonin \"LOJË E RE\"\n")
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {}
+                        }) .show();
+            }
+        });
+
+    }
+
+}
